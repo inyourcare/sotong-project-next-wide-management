55c80e1433dac4ca7515cc30ac7f1994e6b0059c
kwonkihoon <inyourcare@sotongfive.kr>
2022-10-28 16:52:31 +0900

    theme 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,66 @@
 import type { AppProps } from 'next/app'
+import { Provider } from "react-redux"
+import { ThemeProvider } from "@material-ui/styles"
+import CssBaseline from "@material-ui/core/CssBaseline"
+
+import green from "@material-ui/core/colors/green"
+import grey from "@material-ui/core/colors/grey"
+import { createMuiTheme } from "@material-ui/core/styles"
+
+//////// Theme ///////
+const MuiTheme = createMuiTheme({
+  palette: {
+    primary: {
+      light: grey[700],
+      main: grey[800],
+      dark: grey[900],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+})
+////////////////////////
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={MuiTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+
+
+// type Props = {
+//   Component: React.Component
+//   store: any
+// }
+
+// class MyApp extends App<Props> {
+//   componentDidMount() {
+//     // Remove the server-side injected CSS.
+//     const jssStyles = document.querySelector("#jss-server-side")
+//     jssStyles?.parentNode?.removeChild(jssStyles)
+//   }
+
+//   render() {
+//     const { store, Component, pageProps } = this.props
+
+//     return (
+//       <Provider store={store}>
+//         <ThemeProvider theme={MuiTheme}>
+//           {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+//           <CssBaseline />
+//           <Component {...pageProps} />
+//         </ThemeProvider>
+//       </Provider>
+//     )
+//   }
+// }
+
+// export default withRedux(makeStore, {
+//   debug: false,
+// })(MyApp)
