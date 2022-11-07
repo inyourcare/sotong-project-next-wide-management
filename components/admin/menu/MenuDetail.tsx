fe380866503e6274f4080c7edf47d1791ad1f699
kwonkihoon <inyourcare@sotongfive.kr>
2022-11-07 17:19:21 +0900

    챠크라 스택 활용

--- components/admin/menu/MenuDetail.tsx
+++ components/admin/menu/MenuDetail.tsx
@@ -3,20 +3,24 @@ import Router from "next/router";
 import ReactMarkdown from "react-markdown";
 import { Text, Heading, Box } from "@chakra-ui/react";
 import { TMenu } from "@core/types/TMenu";
+import { Stack, HStack, VStack } from '@chakra-ui/react'
 
 const MenuDetail: React.FC<{ menu: TMenu }> = ({ menu }) => {
   const creatorId = menu.creatorId
   return (
     <Box
-      p={5}
+      p={1}
       border="1px solid"
       borderColor="gray.500"
       borderRadius="md"
       onClick={() => Router.push("/menu/[id]", `/menu/${menu.id}`)}
+      w="100%"
     >
-      <Heading size="md">{menu.name}</Heading>
-      <Text fontSize="sm">By {creatorId}</Text>
-      <ReactMarkdown children={menu.greetings} />
+      <Stack spacing={8} direction='row'>
+        <Heading size="md">{menu.name}</Heading>
+        <Text fontSize="sm">By {creatorId}</Text>
+        <ReactMarkdown children={menu.greetings} />
+      </Stack>
     </Box>
   );
 };
--- components/admin/menu/MenuList.tsx
+++ components/admin/menu/MenuList.tsx
@@ -17,18 +17,20 @@ const MenuList: React.FC<MenuProps> = ({ props, data }) => {
     const { t } = useTranslation('menu');
 
     return (
-        <Flex minH={"100vh"} w={"full"} align={"center"} justify={"center"}>
-            <Stack w={"full"} spacing={8} mx={"auto"} py={12} px={6}>
-                <Stack align={"center"}>
+        <Flex minH={"100vh"} w={"100%"} align={"center"} justify={"center"}>
+            <Stack w={"100%"}  spacing={20} mx={"auto"} py={12} px={20}>
+                <Stack w={"full"} align={"center"}>
                     <Heading mb={6}>{t('heading')}</Heading>
                     <Box
                         marginTop={{ base: "1", sm: "5" }}
                         display="flex"
                         flexDirection={{ base: "column", sm: "row" }}
                         justifyContent="space-between"
+                        // w={"full"}
+                        w={"100%"}
                     >
                         {/* {props?.data?.csrfToken} */}
-                        <UnorderedList size="lg">
+                        <UnorderedList size="lg" w={"100%"}>
                             {data?.menus.map((menu) => (
                                 <ListItem key={String(menu.id)}>
                                     <MenuDetail menu={menu} />
