d9bbe41d9bebbd8fc55be690a5ae6360ca46bc46
kwonkihoon <inyourcare@sotongfive.kr>
2022-11-22 10:42:09 +0900

    유지보수 링크 연결

--- core/reference.ts
+++ core/reference.ts
@@ -1,13 +1,17 @@
 // add pages which you want to the array.
 // and make the file in the page folder.
 import { Role } from '@prisma/client'
-
-export const routes = [
+export type Route = {
+    name: string,
+    link: string,
+    authorized?: Role[]
+}
+export const routes: Route[] = [
     { name: "Home", link: "/" },
     { name: "SignIn", link: "/auth/signin" },
     { name: "SignUp", link: "/auth/signup" },
-    { name: "유지보수관리", link: "/boards/maintanance", authorized: [Role.ADMIN,Role.USER]},
-    { name: "메뉴관리", link: "/admin/menu", authorized: [Role.ADMIN]},
-    { name: "파로스", link: "/paros/main"},
+    { name: "유지보수관리", link: "/boards/maintanance", authorized: [Role.ADMIN, Role.USER] },
+    { name: "메뉴관리", link: "/admin/menu", authorized: [Role.ADMIN] },
+    { name: "파로스", link: "/paros/main" },
     // { name: "path name", link: "link url" }, like this
 ];
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,25 @@ import { logger } from "@core/logger";
 import { useEffect } from "react";
 import { prisma } from "@core/prisma"
 import KakaoShareBtn from "@components/common/KakaoShareBtn";
+import { Box, Typography } from "@mui/material";
+import { Route, routes } from "@core/reference";
+import { AppProps } from "next/app";
 
-
-export default function Home({ }) {
+type HomeProps = AppProps & {
+  signInPath: Route | undefined
+}
+export default function Home(props: HomeProps) {
   // const { t } = useTranslation('common');
   const session = useSession();
+  const path = routes;
+  const signInPath = path.filter(({ name, link }) => name === 'SignIn').pop()
+  const maintanancePath = path.filter(({ name, link }) => name === '유지보수관리').pop()
   // const classes = useStyles();
+  // const signInPath = props.signInPath;
   useEffect(() => {
     logger.debug("session change", session, session.data?.user, session.data?.expires)
-  }, [session])
+    logger.debug("props", props)
+  }, [session, props])
   return (<>
     <Flex
       minH={"100vh"}
@@ -37,16 +47,27 @@ export default function Home({ }) {
           <Heading fontSize={"4xl"} textAlign={"center"}>
             메인페이지
           </Heading>
-          <Text fontSize={"lg"} color={"gray.600"}>
-            {session.status === 'authenticated'
-              && <Button
-                onClick={() => { signOut() }}
-              // className={`${classes.signIn_Btn} ${classes.width100P}`}
-              >
-                {"next auth 로그아웃"}
-              </Button>}
-            {session.data?.expires}하이루?(Role:{session.data?.user?.role}){session.data?.user?.name}<br />
-          </Text>
+          {session.status !== 'authenticated'
+            // && <Link href={signInPath?.link as string}>
+            && <Box>
+              <Link href={signInPath?.link as string}>
+                <Typography color="inherit">Login</Typography>
+              </Link>
+            </Box>}
+          {session.status === 'authenticated'
+            && (<Box><Button
+              onClick={() => { signOut() }}
+            // className={`${classes.signIn_Btn} ${classes.width100P}`}
+            >
+              {"next auth 로그아웃"}
+            </Button>
+
+              <Link href={maintanancePath?.link as string}>
+                <Typography color="inherit">유지보수관리</Typography>
+              </Link>
+            </Box>
+            )}
+          {session.data?.expires}하이루?(Role:{session.data?.user?.role}){session.data?.user?.name}<br />
           <KakaoShareBtn />
         </Stack>
       </Stack>
@@ -66,19 +87,22 @@ export default function Home({ }) {
 
 export const getStaticProps: GetStaticProps<any> = async (context) => {
   const { locale } = context;
+  const path = routes;
+  const signInPath = path.filter(({ name, link }) => name === 'SignIn').pop()
+  console.log('getStaticProps', signInPath)
   if (locale) {
     const result = await serverSideTranslations(locale, ["common"]);
     return {
       props: {
         // common 은 위 locales/ko 아래에 만든 json 파일 명이다. 다른 파일을 사용한다면 바꿔주자.
         ...(result),
+        signInPath
       },
     }
   }
   console.error('locale undefind', locale)
   return {
     props: {
-
     }
   }
 };
