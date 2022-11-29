11a55c2f7a831b2d7fdde12dbc493f89cf582798
kwonkihoon <inyourcare@sotongfive.kr>
2022-11-29 10:01:37 +0900

    에러 페이지 디자인 변경

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,13 +1,32 @@
+import { Box, Button, CssBaseline, Grid } from "@mui/material"
 import { NextApiResponse } from "next"
 import { ErrorProps } from "next/error"
+import Link from "next/link"
 
 function Error({ statusCode }: { statusCode: string }) {
     return (
-        <p>
-            {statusCode
-                ? `An error ${statusCode} occurred on server`
-                : 'An error occurred on client'}
-        </p>
+        <>
+            <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <Grid container>
+                    <Grid item xs={12}>
+                        <Box display="flex" justifyContent="center" alignItems={'center'}>
+                            <p>
+                                {statusCode
+                                    ? `An error ${statusCode} occurred on server`
+                                    : 'An error occurred on client'}
+                            </p>
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box display="flex" justifyContent="center" alignItems={'center'}>
+                            <Link color={"blue.400"} href="/">
+                                Home
+                            </Link>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+        </>
     )
 }
 
